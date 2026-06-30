fdbeb1d1b9e4f5473f2c2ff5f098f9143ef630e5
abhinavappu46 <abhinav@gmail.com>
2026-06-30 23:50:13 +0530

    4th commit

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const ConsultationRoute=require("./Routes/Consultation");
 const app=express();
 
 
-
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 app.use(express.json());
 app.use(cors({
     origin:"https://prakriti-interior-design.onrender.com"
