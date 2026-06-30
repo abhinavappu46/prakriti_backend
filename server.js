315ad61aaaee63bcdfac9a17cdf9a72fb59ae1da
abhinavappu46 <abhinav@gmail.com>
2026-06-30 21:34:07 +0530

    orgin added

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app=express();
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"https://prakriti-interior-design.onrender.com"
+}));
 app.use("/consultation",ConsultationRoute);
 
 
